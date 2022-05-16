6ddea45fac8c3d49eb0adff109e2fe3b17c01127
Pearlitic <75706622+Pearlitic@users.noreply.github.com>
2022-05-16 15:46:50 +0800

    Update main.js

--- main.js
+++ main.js
@@ -21,6 +21,65 @@ function calcIED(arr) {
 }
 // optimize! button begins calculation
 function calc() {
+	var names = []
+
+	display_top = document.getElementById("disTop").value;
+
+	var loop_div = document.getElementById("loop");
+	document.getElementById('loop').innerHTML = "";
+
+	for (i = 1; i <= display_top; i++) {
+		//final dmg spam
+		window['span' + i] = document.createElement("span");
+		window['span' + i].setAttribute('id', 'fd' + i);
+		//final dmg text
+		var b = document.createElement("b");
+		b.textContent = i + ' - FD: ';
+		window['span' + i].appendChild(b);
+		loop_div.appendChild(window['span' + i]);
+		//final dmg percent op_f
+		window['op' + i + 'f'] = document.createElement("span");
+		window['op' + i + 'f'].setAttribute('id', 'op' + i + 'f');
+		window['op' + i + 'f'].textContent = '000%';
+		loop_div.appendChild(window['op' + i + 'f']);
+		//next line
+		var br = document.createElement("br");
+		loop_div.appendChild(br);
+		//adi span
+		window['span' + i + 'adi'] = document.createElement("span");
+		window['span' + i + 'adi'].setAttribute('id', 'span' + i + 'adi');
+		//add a/d/i to adi span
+		var b = document.createElement("b");
+		b.textContent = '\u00A0\u00A0A/D/I: ';
+		window['span' + i + 'adi'].appendChild(b);
+		loop_div.appendChild(window['span' + i + 'adi']);
+		//adi result span
+		window['op' + i + 'adi'] = document.createElement("span");
+		window['op' + i + 'adi'].setAttribute('id', 'op' + i + 'adi');
+		window['op' + i + 'adi'].textContent = '(0,0,0)';
+		loop_div.appendChild(window['op' + i + 'adi']);
+
+		//p span
+		window['span' + i + 'p'] = document.createElement("span");
+		window['span' + i + 'p'].setAttribute('id', 'span' + i + 'p');
+		//add p to p span
+		var b = document.createElement("b");
+		b.textContent = '\u00A0\u00A0P: ';
+		window['span' + i + 'p'].appendChild(b);
+		loop_div.appendChild(window['span' + i + 'p']);
+		//p result span
+		window['op' + i + 'p'] = document.createElement("span");
+		window['op' + i + 'p'].setAttribute('id', 'op' + i + 'p');
+		window['op' + i + 'p'].textContent = '(0,0,0)';
+		loop_div.appendChild(window['op' + i + 'p']);
+
+
+		//new line
+		var br = document.createElement("br");
+		loop_div.appendChild(br);
+
+	}
+
 	//get user input values
 	inATK = document.getElementById("attackEl").value;
 	inDMG = document.getElementById("damageEl").value;
@@ -28,6 +87,7 @@ function calc() {
 	inFam = document.getElementById("famEl").value;
 	inPDR = document.getElementById("pdrEl").value;
 	inPrime = document.getElementById("primeEl").value;
+
 	//convert ied string to array
 	inIEDList = inIED.split `,`.map((x) => +x);
 	//check input, set box red if not compliant
@@ -86,13 +146,14 @@ function calc() {
    }*/
 	//console.table(final);
 	// print values into the web page
-	for (r = 0; r < 5; r++) {
+	for (r = 0; r < display_top; r++) {
 		tmp = r + 1;
-		document.getElementById("op" + tmp + "f").textContent = Math.round(final[r][6] * 100) + "%";
-		document.getElementById("op" + tmp + "a").textContent = final[r][0];
-		document.getElementById("op" + tmp + "d").textContent = final[r][1];
-		document.getElementById("op" + tmp + "i").textContent = final[r][2];
-		document.getElementById("op" + tmp + "p").textContent = final[r][3] + "," + final[r][4] + "," + final[r][5];
+		document.getElementById("op" + tmp + "f").textContent = Math.round(final[r][0] * 100) + "%";
+		//document.getElementById("op" + tmp + "a").textContent = final[r][0];
+		//document.getElementById("op" + tmp + "d").textContent = final[r][1];
+		//document.getElementById("op" + tmp + "i").textContent = final[r][2];
+		document.getElementById("op" + tmp + "adi").textContent = '(' + final[r][1][0] + "," + final[r][1][1] + "," + final[r][1][2] + ')';
+		document.getElementById("op" + tmp + "p").textContent = '(' + final[r][1][3] + "," + final[r][1][4] + "," + final[r][1][5] + ')';
 	}
 	//console.log("\nA: " + final[0][0] + "  D: " + final[0][1] + "  I: " + /*inIEDList.toString() + "  " +*/ final[0][2] + " | P: (" + final[0][3] + "," + final[0][4] + "," + final[0][5] + ") \tFD: " + Math.round(final[0][6] * 100) + "%");
 }
@@ -110,11 +171,7 @@ function optimize(opA, opD, opI, opF, opPDR, opPR) {
 	//oFD = 0;
 	//initialize array for final values
 	FDc = 0;
-	final1 = [0, 0, 0, 0, 0, 0, 0];
-	final2 = [0, 0, 0, 0, 0, 0, 0];
-	final3 = [0, 0, 0, 0, 0, 0, 0];
-	final4 = [0, 0, 0, 0, 0, 0, 0];
-	final5 = [0, 0, 0, 0, 0, 0, 0];
+	var table = {};
 	//nested for loop galore to figure out top 5
 	for (let a = 0; a <= oF; a++) {
 		for (let d = 0; d <= oFd; d++) {
@@ -128,31 +185,18 @@ function optimize(opA, opD, opI, opF, opPDR, opPR) {
 								fD = 0.3 * (d - pd) + 0.4 * pd + opD;
 								fI = calcIED(Array(pi).fill(0.4).concat(Array(i - pi).fill(0.3), [opI]));
 								FD = calcFD(fA, fD, fI, opPDR);
-								if (FD > FDc) {
-									FDc = FD;
+								while (true) {
+									if (!(FD in table)) {
+										table[FD] = [a, d, i, pa, pd, pi]
+										break;
+									}
+									FD += 0.0000000001
 								}
+
+
+
 								///
-								if (FD > final1[6]) {
-									final5 = final4;
-									final4 = final3;
-									final3 = final2;
-									final2 = final1;
-									final1 = [a, d, i, pa, pd, pi, FD];
-								} else if (FD > final2[6]) {
-									final5 = final4;
-									final4 = final3;
-									final3 = final2;
-									final2 = [a, d, i, pa, pd, pi, FD];
-								} else if (FD > final3[6]) {
-									final5 = final4;
-									final4 = final3;
-									final3 = [a, d, i, pa, pd, pi, FD];
-								} else if (FD > final4[6]) {
-									final5 = final4;
-									final4 = [a, d, i, pa, pd, pi, FD];
-								} else if (FD > final5[6]) {
-									final5 = [a, d, i, pa, pd, pi, FD];
-								}
+
 								//console.log("A: " + a + "  D: " + d + "  I: " + i + " | P: (" + pa + "," + pd + "," + pi + ") \tFD: " + Math.round(FD * 100) + "%");
 							}
 						}
@@ -162,7 +206,14 @@ function optimize(opA, opD, opI, opF, opPDR, opPR) {
 		}
 	}
 	//return [oA, oD, oI, oPrA, oPrD, oPrI, oFD]
-	return [final1, final2, final3, final4, final5];
+
+	var sorted = Object.keys(table).map(function(key) {
+		return [key, table[key]];
+	});
+	sorted.sort(function(first, second) {
+		return second[0] - first[0];
+	});
+	return sorted;
 }
 //calculate final damage % to a boss accounting for pdr
 function calcFD(a, d, i, pdr) {
@@ -174,6 +225,9 @@ function calcFD(a, d, i, pdr) {
 	}
 	return (1 + a) * (1 + d) * DM;
 }
+
+
+
 /////////////////////////////////////////////////////////////////////////////
 function showSpoiler(obj) {
 	var inner = obj.parentNode.getElementsByTagName("div")[0];
