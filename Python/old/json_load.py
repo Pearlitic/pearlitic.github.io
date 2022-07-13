7ef5f50f00a83689cfa4ff6d45938f605177bb18
Pearlitic <75706622+Pearlitic@users.noreply.github.com>
2022-07-13 15:04:42 +0800

    Update json_load.py

--- Python/old/json_load.py
+++ Python/old/json_load.py
@@ -3,3 +3,12 @@ url = "https://raw.githubusercontent.com/Pearlitic/pearlitic.github.io/main/%25/
 resp = requests.get(url)
 data = json.loads(resp.text)
 print(data)
+
+import pandas as pd
+
+dff = pd.concat({
+        k: pd.DataFrame.from_dict(v, 'index') for k, v in data.items()
+    }, 
+    axis=0)
+
+df = pd.DataFrame(data.items(), columns=["Char", "age"]) 
